fb21d2bcd1933df6299e10b7684b88983114f49c
Evans Shango <evanshango@github.com>
2019-09-29 16:14:48 +0300

    refactor code and add jwt authentication create a separate directory for frontend with react

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,45 +1,63 @@
-const { buildSchema } = require('graphql')
+const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-        type Event{
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
+    type Booking{
+        _id: ID!
+        event: Event!
+        user: User!
+        createdAt: String!
+        updatedAt: String!
+    }
 
-        type User {
-          _id: ID!
-          email: String!
-          password: String
-          createdEvents: [Event!]
-        }
+    type Event{
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        creator: User!
+    }
 
-        input UserInput {
-          email: String!
-          password: String!
-        }
+    type User {
+        _id: ID!
+        email: String!
+        password: String
+        createdEvents: [Event!]
+    }
 
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
+    type AuthData{
+        userId: ID!
+        token: String!
+        tokenExpiration: Int!
+    }
 
-        type RootQuery{
-            events: [Event!]!
-        }
-        
-        type RootMutation{
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
+    input UserInput {
+        email: String!
+        password: String!
+    }
 
-        schema{
-            query: RootQuery
-            mutation: RootMutation 
-        }
-    `)
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    type RootQuery{
+        events: [Event!]!
+        bookings: [Booking!]!
+        login(email: String!, password: String!): AuthData!
+    }
+    
+    type RootMutation{
+        createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
+        bookEvent(eventId: ID!): Booking!
+        cancelBooking(bookingId: ID!): Event!
+    }
+
+    schema{
+        query: RootQuery
+        mutation: RootMutation 
+    }
+`);
